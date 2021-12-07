a351db6672adc9b1101d2f29b4dc64c38760ac85
dsterna <dsterna@kth.se>
2021-12-07 16:20:12 +0100

    nr7

--- adv7/adv7.js
+++ adv7/adv7.js
@@ -2,20 +2,29 @@ import { data1, testData } from "./input7.js";
 
 const data = testData;
 
+const min = Math.min(...data);
+const max = Math.max(...data);
 
-const min = Math.min(...data)
-const max = Math.max(...data)
+let sum = Infinity;
+let point = 0;
 
-console.log(min, max)
-let sum = Infinity
-let point = 0
-for (let i = 0; i < 16; i++) {
-  let diff = 0
+let sums = [];
+for (let i = min; i < max; i++) {
+  let nr1Diff = 0;
+  let nr2Diff = 0;
   for (let ii = 0; ii < data.length; ii++) {
-    diff = diff + Math.abs(i - data[ii])
+    let diffCopy = Math.abs(i - data[ii]);
+    nr1Diff = nr1Diff + diffCopy;
+    for (let iii = 1; iii < diffCopy + 1; iii++) {
+      nr2Diff = nr2Diff + iii;
+    }
   }
-  if (diff < sum)
-    sum = diff
-    point = i
+  sums.push(nr2Diff);
+  if (nr1Diff < sum) {
+    sum = nr1Diff;
+    point = i;
+  }
 }
-console.log(sum, point)
+console.log(sum)
+sums.sort((a, b) => a - b);
+console.log(sums[0]);
